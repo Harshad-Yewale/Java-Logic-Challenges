cc5faee797244545af732afbeaca4cdb032371a1
Harshad Yewale <harshadyewale73@gmail.com>
2025-12-17 18:23:01 +0530

    Solved Phase-1/Level4 Q8,Q9,Q10 (Done with level 4)

---
+++ src/com/Phase_1_Conditional_Thinking/level_4/Q10.java
@@ -0,0 +1,29 @@
+package com.Phase_1_Conditional_Thinking.level_4;
+
+import java.util.Scanner;
+
+public class Q10 {
+
+    //Q Take a password string and check basic rules (length ≥ 8 and contains at least one digit).
+
+    public static void main(String[] args) {
+
+        //declare variable
+        String password;
+        Scanner sc=new Scanner(System.in);
+
+        //take user input
+        System.out.print("plz Set your password : ");
+        password =sc.nextLine();
+
+        if(password.length()>=8 && password.matches(".*\\d.*")){
+            System.out.println("Password Set Successfully\nYour Password :"+password);
+        }
+        else if(password.length()>=8){
+            System.out.println("Password must contain at least one digit");
+        }
+        else {
+            System.out.println("Password must be at least 8 characters long");
+        }
+    }
+}
